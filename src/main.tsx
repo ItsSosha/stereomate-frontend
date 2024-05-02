2fde237f37f9547824f2753deaede7f616e14f22
ItsSosha <whoissosha@gmail.com>
2024-05-02 21:52:57 +0200

    chore: add fonts, mixins and normalize

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider } from "react-router-dom";
 
 import { router } from "@/utils/router";
 
+import "./normalize.css";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
